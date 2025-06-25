4c6dddd1881f214e2de95d042acdea31809a629a
André Ricardo <contatoadmx@gmail.com>
2025-06-24 22:35:12 -0300

    BR Status message

---
+++ helper/script/env.php
@@ -0,0 +1,16 @@
+<?php
+
+use PhpMx\Env;
+
+Env::default('STM_200', 'ok');
+Env::default('STM_201', 'criado');
+Env::default('STM_204', 'sem conteúdo');
+Env::default('STM_303', 'redirecionamento');
+Env::default('STM_400', 'requisição inválida');
+Env::default('STM_401', 'não autorizado');
+Env::default('STM_403', 'proibido');
+Env::default('STM_404', 'não encontrado');
+Env::default('STM_405', 'método não permitido');
+Env::default('STM_500', 'erro interno do servidor');
+Env::default('STM_501', 'não implementado');
+Env::default('STM_503', 'serviço indisponível');
